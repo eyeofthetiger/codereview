cdf379d8fb5b39e50f530ab444d1dd1531721383
Luke Hansford <l.s.hansford@gmail.com>
2014-10-23 09:16:55 +1000

    Date input field should work

--- review/forms.py
+++ review/forms.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from django import forms
+from django.utils import timezone
 
 from review.models import Assignment, EmailPreferences, Question
 
@@ -47,28 +50,46 @@ class AssignmentForm(forms.Form):
 
 	assignment_id = forms.CharField(max_length=140) #Unique ID for assignment
 	name = forms.CharField(max_length=140)
-	open_date = forms.DateTimeField()
-	due_date = forms.DateTimeField()
+	open_date = forms.CharField()
+	due_date = forms.CharField()
 	description_raw = forms.CharField(label='Description', widget=forms.Textarea)
 	allow_multiple_uploads = forms.BooleanField(required=False)
 	allow_help_centre = forms.BooleanField(required=False)
 	number_of_peer_reviews = forms.IntegerField()
-	review_open_date = forms.DateTimeField()
-	review_due_date = forms.DateTimeField()
+	review_open_date = forms.CharField()
+	review_due_date = forms.CharField()
 	weighting = forms.IntegerField()
 	test_zip = forms.FileField(required=False)
 	dockerfile = forms.FileField(required=False)
 	docker_command = forms.CharField(required=False)
 	test_required = forms.BooleanField(required=False)
 
-	def due_open_date(self):
-		print self
+	def format_date(self, date_str):
+		""" Takes a string in the format DD-MM-YYYY HH:MM and converts it to 
+			datetime format. """
+		d = datetime.strptime(date_str, "%d-%m-%Y %H:%M")
+		return timezone.make_aware(d, timezone.get_current_timezone())
+
+	def clean_due_date(self):
+		""" Convert due date representation to format readable by Django. """
 		data = self.cleaned_data['due_date']
-		print data
-		return data
+		return self.format_date(data)
 
 	def clean_open_date(self):
-		print self
+		""" Convert open date representation to format readable by Django. """
 		data = self.cleaned_data['open_date']
-		print data
-		return data
+		return self.format_date(data)
+
+	def clean_review_due_date(self):
+		""" Convert review due date representation to format readable by 
+			Django. 
+		"""
+		data = self.cleaned_data['review_due_date']
+		return self.format_date(data)
+
+	def clean_review_open_date(self):
+		""" Convert review open date representation to format readable by 
+			Django. 
+		"""
+		data = self.cleaned_data['review_open_date']
+		return self.format_date(data)
